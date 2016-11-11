fb3be49d1b700854d60fdc8284a92c158f9e8b29
filiponegrao <filiposilver@hotmail.com>
2016-11-11 18:34:13 -0200

    agora ta funcionando

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,7 +2,6 @@ package controller;
 
 public class Main 
 {
-	
 	public static void main(String[] args) 
 	{
 		Server.sharedInstance().connect(6969);
@@ -12,5 +11,4 @@ public class Main
 		    }
 		}));
 	}
-
 }
